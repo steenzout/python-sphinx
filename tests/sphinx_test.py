f74e23ac518aceb8b832c27a1385f1010827081a
Pedro Salgado <steenzout@ymail.com>
2016-10-14 11:59:06 -0600

    added ResourceGeneratorTestCase._compare_output() function. improved test for conf() and makefile().

--- tests/sphinx_test.py
+++ tests/sphinx_test.py
@@ -9,11 +9,26 @@ from steenzout.sphinx import ResourceGenerator
 class ResourceGeneratorTestCase(unittest.TestCase):
     """Tests for the steenzout.sphinx.ResourceGenerator class."""
 
+    @staticmethod
+    def _compare_output(expected, output):
+
+        assert output is not None
+
+        content = None
+        with open('tests/resources/%s' % expected, 'r') as fd:
+            content = fd.read()
+
+        assert content == output
+
     def setUp(self):
         self.generator = ResourceGenerator('steenzout', 'steenzout.sphinx')
 
     def test_conf(self):
-        assert self.generator.conf() is None
+        conf = self.generator.conf()
+
+        self._compare_output('conf.py', conf)
 
     def test_makefile(self):
-        assert self.generator.makefile() is None
+        makefile = self.generator.makefile()
+
+        self._compare_output('Makefile', makefile)
