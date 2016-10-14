03242180978b189891b93639b7e0c8ef842a5182
Pedro Salgado <steenzout@ymail.com>
2016-10-14 11:25:33 -0600

    added license header. updated module docstring.

--- steenzout/sphinx/metadata.py
+++ steenzout/sphinx/metadata.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
-"""Package metadata."""
+#
+# Copyright 2016 Pedro Salgado
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Package metadata module."""
 
 __author__ = u'Pedro Salgado'
 __author_email__ = u'steenzout@ymail.com'
