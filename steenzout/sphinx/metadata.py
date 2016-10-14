d6ec5f529715b91a698eaad300051c33e1429833
Pedro Salgado <steenzout@ymail.com>
2016-10-14 09:16:29 -0600

    added metadata module.

---
+++ steenzout/sphinx/metadata.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Package metadata."""
+
+__author__ = u'Pedro Salgado'
+__author_email__ = u'steenzout@ymail.com'
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: Documentation',
+    'Topic :: Documentation :: Sphinx',
+    'Topic :: Software Development :: Documentation'
+]
+__copyright__ = u'2016, %s' % __author__
+
+__description__ = u'Sphinx configuration for the steenzout GitHub organization.'
+
+__maintainer__ = u'Pedro Salgado'
+__maintainer_email__ = u'steenzout@ymail.com'
+
+__url__ = u'https://github.com/steenzout/python-sphinx/'
+
+__version__ = u'0.0.1'
