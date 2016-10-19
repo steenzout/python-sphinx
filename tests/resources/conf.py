8c75f1ee8203b670bac3564160e303a74ae49df4
Pedro Salgado <steenzout@ymail.com>
2016-10-19 09:48:21 -0600

    update test resource.

--- tests/resources/conf.py
+++ tests/resources/conf.py
@@ -38,7 +38,7 @@ from recommonmark.parser import CommonMarkParser
 
 
 _package = 'steenzout.sphinx'
-_version = semantic_version.Version('1.0.4')
+_version = semantic_version.Version('1.0.8')
 
 # -- General configuration ------------------------------------------------
 
