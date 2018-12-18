9021ff60e9d6d59ec5bb39fcf4df2c046f82f324
Pedro Salgado <steenzout@users.noreply.github.com>
2018-12-18 13:52:54 -0700

    updated copyright year range.

--- steenzout/sphinx/metadata.py
+++ steenzout/sphinx/metadata.py
@@ -36,7 +36,7 @@ __classifiers__ = [
     'Topic :: Documentation :: Sphinx',
     'Topic :: Software Development :: Documentation'
 ]
-__copyright__ = u'2016, %s' % __author__
+__copyright__ = u'2016-2018, %s' % __author__
 
 __description__ = u'Sphinx configuration for the steenzout GitHub organization.'
 
